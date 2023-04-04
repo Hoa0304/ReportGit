b32f8f8e5cc174866d5d79a9a55c93df2aa1a4b0
HoaDB <117150692+Hoa0304@users.noreply.github.com>
2023-04-04 16:23:09 +0700

    04/04/2023

---
+++ Practice/Calculator.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace trainningCong.git.ReportGit.Practice
+{
+    public class Calculator
+ {
+ public int Add(int x, int y)
+ {
+ return x + y;
+ }
+ public int Subtract(int x, int y)
+ {
+ return x + y;
+ }
+ public int Multiply(int x, int y)
+ {
+ return x * y;
+ }
+ public int Divide(int x, int y)
+ {
+ return x / y;
+ }
+ }
+}
