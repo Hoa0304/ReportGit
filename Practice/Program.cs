e122d86231b2c24c009c735dec9147ea7f20aa3e
HoaDB <117150692+Hoa0304@users.noreply.github.com>
2023-04-11 15:57:17 +0700

    11/04

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-hfdsazfazfdzfdzfdzg
 namespace trainningCong.git.ReportGit.Practice
 {
     public static class Program
